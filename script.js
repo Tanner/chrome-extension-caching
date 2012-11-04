ce493c9e74d2a135fa7688bf7291f9ad8becaea3
Tanner Smith <tanner@tssoftware.net>
2012-11-04 16:00:37 -0500

    Add a JS message from the script itself.

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 chrome.extension.sendMessage({message: "Hello"}, function(response) {
 	document.getElementById("message").innerHTML = response.message;
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+	document.getElementById("me").innerHTML = "This is a message from myself.";
+}, false );
